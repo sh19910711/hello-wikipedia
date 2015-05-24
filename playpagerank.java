d55cd9d28731f871c34a06fbc65e410e11a82365
Hiroyuki Sano <sh19910711@gmail.com>
2015-05-24 13:31:40 +0900

    edit playpagerank.java

--- playpagerank.java
+++ playpagerank.java
@@ -2,18 +2,60 @@ import java.io.*;
 import java.util.*;
 
 class playpagerank {
-  public static void main(String args[]) throws IOException {
-    File filePageranks = new File("./wikipedia-pageranks.raw");
-    double[] pageranks = new double[(int)(filePageranks.length() / 8)];
-    DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(filePageranks)));
+
+  double[] pageranks;
+  Map<String, Integer> idByTitle;
+  Map<Integer, String> titleById;
+
+  void initPageranks() throws IOException {
+    File f = new File("./wikipedia-pageranks.raw");
+    pageranks = new double[(int)(f.length() / 8)];
+    DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(f)));
     for ( int i = 0; i < pageranks.length; ++ i ) {
       pageranks[i] = in.readDouble();
     }
-    System.out.printf("日本: " + 1864744 + ": %.3f\n", -Math.log10(pageranks[1864744]));
-    System.out.printf("東京都: " + 774362 + ": %.3f\n", -Math.log10(pageranks[774362]));
-    System.out.printf("アメリカ合衆国: " +1698838 + ": %.3f\n", -Math.log10(pageranks[1698838]));
-    System.out.printf("英語: " + 3377 + ": %.3f\n", -Math.log10(pageranks[3377]));
-    
+  }
+
+  void initHash() throws IOException {
+    File f = new File("./wikipedia-pagerank-page-id-title.raw");
+    idByTitle = new HashMap<String, Integer>();
+    titleById = new HashMap<Integer, String>();
+    Scanner s = new Scanner(new BufferedInputStream(new FileInputStream(f)));
+    while ( s.hasNext() ) {
+      String title = s.next();
+      Integer id = s.nextInt();
+      idByTitle.put(title, id);
+      titleById.put(id, title);
+    }
+  }
+
+  double getPagerank(Integer id) {
+    return -Math.log10(pageranks[id]);
+  }
+
+  Integer findIdByTitle(String title) {
+    return idByTitle.get(title);
+  }
+
+  void showPagerank(String title) {
+    Integer id = findIdByTitle(title);
+    System.out.printf("%s(%d): %.3f\n", title, id, getPagerank(id));
+  }
+
+  playpagerank() throws IOException {
+    initPageranks();
+    initHash();
+
+    int cnt = 0;
+    for ( Map.Entry<Integer, String> e : titleById.entrySet() ) {
+      if ( cnt >= 100 ) break;
+      System.out.println(e.getValue());
+      cnt ++;
+    }
+  }
+
+  public static void main(String args[]) throws IOException {
+    new playpagerank();
   }
   
 }
